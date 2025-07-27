d538a477e3d4faa6f046d1925c8c22fdc7a4f02c
LIAOLTHOHENG <32264191+LIAOLTHOHENG@users.noreply.github.com>
2025-07-27 17:28:35 +0800

    sql更新

--- jnu_forum-master/src/test/resources/常用.sql
+++ jnu_forum-master/src/test/resources/常用.sql
@@ -1,2 +1,3 @@
 -- 孤独星球
-select utr .description ,sb.name,utr .`date`  from user_tag_relation utr left join stock_basic sb  on utr.symbol =sb.symbol  where FTagId =17 order by `date` desc;
+select utr .description ,utr .symbol ,sb.name,sb.industry ,utr .`date`  from user_tag_relation utr left join stock_basic sb  on utr.symbol =sb.symbol  where FTagId =17 order by `date` desc;
+
